eb5556f77526df616bd6171298678a24f8ffc0a9
Matias Larsson <larssonmts@gmail.com>
2022-06-10 22:23:29 -0300

    se trabaja sobre header footer y welcome section para mobile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import NavBar from './components/NavBar';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import NavBar from 'components/NavBar';
+import Main from 'components/Main';
+import Footer from 'components/Footer';
 
 function App() {
   return (
