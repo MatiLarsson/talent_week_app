c1809db07913f80315e2feb9f5bcbd47a137485f
Gabriel Da Silva <srgdsm98@gmail.com>
2022-06-30 15:01:05 -0300

    added typeform and responsive to activities section

--- src/components/main-components/activities/index.js
+++ src/components/main-components/activities/index.js
@@ -1,22 +1,27 @@
+import useWindowSize from 'components/hooks/useWindowSize';
 
 export default function Activities () {
+  const [width] = useWindowSize();
+
   return (
     <section className="activities">
-      <img src="/assets/images/mobile/circuito-morado-activities-mobile.png" className="circuito-activities" alt='circuito activities'/>
+      <img src={width >= 1024 ? "/assets/images/desktop/circuito-morado-activities-desktop.png" : "/assets/images/mobile/circuito-morado-activities-mobile.png"} className="circuito-activities" alt='circuito activities'/>
+      <img src={width >= 1024 ? "/assets/images/desktop/vectores-activities-desktop.svg" : "/assets/images/mobile/vectores-activities-mobile.svg"}className="vector-activities" alt='vector activities'/>
+      
       <div className="activities-title">
         DURANTE ESTOS DÍAS PODRÁS:
       </div>
       <div className="activities-container">
       <div className="activities-exposiciones">
-          <p className="exposiciones-title titles"><img src="/assets/logos/webcam.svg" alt='webcam'/>EXPOSICIONES</p>
-          <p className="entrevistas-text text">Asiste a los webinars de todas las empresas participantes</p>
+          <p className="exposiciones-title titles"><img src="/assets/logos/webcam.svg" className='activities-logos' alt='webcam'/>EXPOSICIONES</p>
+          <p className="entrevistas-text text">Asistir a las exposiciones de todas las empresas participantes</p>
         </div>
         <div className="activities-entrevistas">
-          <p className="entrevistas-title titles"><img src="/assets/logos/message-bubble.svg" alt='bubble'/>ENTREVISTAS</p>
+          <p className="entrevistas-title titles"><img src="/assets/logos/message-bubble.svg" className='activities-logos' alt='bubble'/>ENTREVISTAS</p>
           <p className="entrevistas-text text">Participa de entrevistas grupales con las empresas y candidatos preseleccionados</p>
         </div>
         <div className="activities-tips">
-          <p className="tips-title titles"><img src="/assets/logos/lightbulb.svg" alt='lightbulb'/>TIPS</p>
+          <p className="tips-title titles"><img src="/assets/logos/lightbulb.svg" className='activities-logos' alt='lightbulb'/>TIPS</p>
           <p className="tips-text text">Conoce tips que te ayudarán en tu búsqueda laboral</p>
         </div>
       </div>
