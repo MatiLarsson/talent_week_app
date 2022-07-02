874ed32a696641a5f4dcf30fe676a5c0fca41493
Matias Larsson <larssonmts@gmail.com>
2022-07-02 17:26:19 -0300

    restructure sass files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './style.scss'
+import './sass/style.scss'
 import './fonts/Gobold/Gobold_Bold_Italic.otf'
 import './fonts/Gobold/Gobold_Regular_Italic.otf'
 import './fonts/UntitledSans/UntitledSans-Medium.otf'
